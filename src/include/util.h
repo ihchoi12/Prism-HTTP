2ad1939ff229da736fcb1514963caaaf57d3984d
ihchoi12 <inho.choi.12@gmail.com>
2024-10-21 05:16:57 +0000

    minor update

--- src/include/util.h
+++ src/include/util.h
@@ -10,9 +10,8 @@
   do {                                                                         \
     struct timeval _t0;                                                        \
     gettimeofday(&_t0, NULL);                                                  \
-    fprintf(stderr, "%03d.%06d %s [%d] " _fmt, (int)(_t0.tv_sec % 1000),       \
-            (int)_t0.tv_usec, __FUNCTION__, __LINE__, ##__VA_ARGS__);          \
-  } while (0)
+    fprintf(stderr, "\n *** %s [%d] " _fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__);   \
+  } while (0) // fprintf(stderr, "\n %03d.%06d %s [%d]\n" _fmt, (int)(_t0.tv_sec % 1000), (int)_t0.tv_usec, __FUNCTION__, __LINE__, ##__VA_ARGS__);          
 #define assertf(A, M, ...) if(!(A)) {fprintf(stderr, M, ##__VA_ARGS__); assert(A); }
 #else
 #define DEBUG(_fmt, ...)
